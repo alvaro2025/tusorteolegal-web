f6c1dbe8b1fad0763862e5a20a8edc0543b0c8fe
alvaro2025 <diazcifuentess@gmail.com>
2026-03-24 01:08:04 -0300

    feat: add Microsoft Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,19 @@ export default function RootLayout({
             `,
           }}
         />
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "w0lbu92de0");
+            `,
+          }}
+        />
         <Navigation />
         <div className="pt-16">
           {children}
